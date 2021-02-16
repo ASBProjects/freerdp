ecca967fc257fe35abf77e11041c6675d84cc381
Atomic <71532270+ASBProjects@users.noreply.github.com>
2021-02-16 18:36:15 +0100

    Update createRDP.sh

--- createRDP.sh
+++ createRDP.sh
@@ -1,8 +1,8 @@
 printf "I DID NOT MAKE THIS BUT JOIN MY DISCORD: https://discord.gg/jTP9Xj2Puc" >&2
 {
-sudo useradd -m Atomic
-sudo adduser Atomic sudo
-echo 'Atomic:8426' | sudo chpasswd
+sudo useradd -m ALOK
+sudo adduser ALOK sudo
+echo 'ALOK:8426' | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 sudo apt-get update
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
@@ -19,12 +19,12 @@ sudo apt install --assume-yes --fix-broken
 sudo apt install nautilus nano -y 
 sudo apt -y install obs-studio
 sudo apt -y install firefox
-sudo adduser Atomic chrome-remote-desktop
+sudo adduser ALOK chrome-remote-desktop
 } &> /dev/null &&
 printf "\nSetup Completed " >&2 ||
 printf "\nError Occured " >&2
 printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
 read -p "Paste Here: " CRP
-su - Atomic -c """$CRP"""
+su - ALOK -c """$CRP"""
 printf 'Check https://remotedesktop.google.com/access/ \n'
 printf 'Your SUDO Pasword Is 8426 \n'
